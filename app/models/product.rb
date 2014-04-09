5b731d22a76fb59ce8bd92468086a77dc344fcb5
Anthony Dagati <tdagati.private@gmail.com>
2014-04-09 08:51:26 -0700

    Updated models

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, length: {minimum: 10}
+
+	def self.latest
+		Product.order(:updated_at).last
+	end
+
 end
